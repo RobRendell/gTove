82afbf4f0d82704be52ff29ef47fc40faf945216
Bryan McConnahea <transcendrstudio@gmail.com>
2018-10-31 14:50:03 +0100

    Adds firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+
+var fbConfig = {
+    apiKey: "AIzaSyDy-Yo_zDWl2SU7o1RqviKpaAUlURDWh2U",
+    authDomain: "gtove2.firebaseapp.com",
+    databaseURL: "https://gtove2.firebaseio.com",
+    projectId: "gtove2",
+    storageBucket: "gtove2.appspot.com",
+    messagingSenderId: "828285745103"
+};
+
+export default fbConfig;
