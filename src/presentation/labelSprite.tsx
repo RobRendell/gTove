9994e1565554db08e0dc4a4f09ee2da9ca8ac45c
Rob Rendell <rob.rendell.au@gmail.com>
2026-02-21 23:25:07 +1100

    Reduce the clickable area of sprite labels to just the text.

--- src/presentation/labelSprite.tsx
+++ src/presentation/labelSprite.tsx
@@ -5,7 +5,7 @@ import {Color} from 'three';
 import {isColourDark} from '../util/threeUtils';
 
 const LABEL_PX_HEIGHT = 48;
-const ANCHOR = new THREE.Vector2(0.5, 0);
+const DROP_SHADOW_PADDING = 8; // Add some padding around the edges of the text for the drop shadow
 
 interface LabelSpriteProps {
     label: string;
@@ -30,29 +30,27 @@ const LabelSprite: FunctionComponent<LabelSpriteProps> = memo(({
                                                                    fillColour,
                                                                    paddingBottom
                                                                }) => {
-    const [labelWidth, setLabelWidth] = useState(0);
-    const [numLines, setNumLines] = useState(0);
-
     const canvasRef = useRef(document.createElement('canvas'));
-    const texture = useMemo(() => {
-        const texture = new THREE.CanvasTexture(canvasRef.current);
-        texture.encoding = THREE.LinearEncoding;
-        return texture;
-    }, []);
+    const [texture, setTexture] = useState<THREE.CanvasTexture>(new THREE.CanvasTexture(canvasRef.current));
 
-    const setLabelContext = useCallback((context: CanvasRenderingContext2D) => {
-        context.font = font || `bold ${LABEL_PX_HEIGHT}px arial, sans-serif`;
-        context.fillStyle = fillColour || 'rgba(255,255,255,1)';
-        context.shadowBlur = 4;
-        context.shadowColor = isColourDark(new Color(context.fillStyle)) ? 'rgba(255,255,255,1)' : 'rgba(0,0,0,1)';
-        context.lineWidth = 2;
-        context.textBaseline = 'bottom';
-        context.textAlign = 'center';
+    const getLabelContext = useCallback(() => {
+        const context = canvasRef.current.getContext('2d');
+        if (context) {
+            context.font = font || `bold ${LABEL_PX_HEIGHT}px arial, sans-serif`;
+            context.fillStyle = fillColour || 'rgba(255,255,255,1)';
+            context.shadowBlur = 4;
+            context.shadowColor = isColourDark(new Color(context.fillStyle)) ? 'rgba(255,255,255,1)' : 'rgba(0,0,0,1)';
+            context.lineWidth = 2;
+            context.textBaseline = 'bottom';
+            context.textAlign = 'center';
+        }
+        return context;
     }, [fillColour, font]);
-    
-    const splitTextIntoLines = useCallback((context: CanvasRenderingContext2D, text: string, maxWidth?: number): string[] => {
-        const paragraphs = text.split('\n');
-        if (maxWidth === undefined) {
+
+    const labelLines = useMemo(() => {
+        const paragraphs = label.split('\n');
+        const context = getLabelContext();
+        if (!maxWidth || !context) {
             return paragraphs;
         }
         const lines: string[] = [];
@@ -73,52 +71,66 @@ const LabelSprite: FunctionComponent<LabelSpriteProps> = memo(({
             lines.push(currentLine);
         }
         return lines;
-    }, []);
+    }, [getLabelContext, label, maxWidth]);
+
+    const {canvasWidth, canvasHeight} = useMemo(() => {
+        const context = getLabelContext();
+        if (!context) {
+            return {canvasWidth: 1, canvasHeight: 1};
+        }
+        return {
+            canvasWidth: Math.max(10,
+                ...labelLines.map((line) => (context.measureText(line).width + DROP_SHADOW_PADDING))
+            ),
+            canvasHeight: labelLines.length * LABEL_PX_HEIGHT + DROP_SHADOW_PADDING
+        };
+    }, [getLabelContext, labelLines])
 
     useEffect(() => {
-        const context = canvasRef.current.getContext('2d');
+        let context = getLabelContext();
         if (!context) {
-            throw new Error('Failed to get 2d canvas context');
+            return;
         }
-        setLabelContext(context);
-        const labelLines = splitTextIntoLines(context, label, maxWidth);
-        const canvasWidth = Math.max(
-            10, maxWidth ?? 10,
-            ...labelLines.map((line) => (context.measureText(line).width))
-        );
 
-        // Convert world-space padding to canvas pixels
-        const pxPerWorldUnit = LABEL_PX_HEIGHT / labelSize;
-        const paddingPx = (paddingBottom ?? 0) * pxPerWorldUnit;
-        const canvasHeight = labelLines.length * LABEL_PX_HEIGHT + paddingPx;
+        canvasRef.current.width = canvasWidth;
+        canvasRef.current.height = canvasHeight;
 
-        canvasRef.current.width = THREE.MathUtils.ceilPowerOfTwo(canvasWidth);
-        canvasRef.current.height = THREE.MathUtils.ceilPowerOfTwo(canvasHeight);
-        // Reapply the context after resizing the canvas (which clears it)
-        setLabelContext(context);
-        const labelOffset = canvasRef.current.height - canvasHeight;
+        // Resizing the canvas clears the context, reapply.
+        context = getLabelContext()!;
         labelLines.forEach((line, index) => {
-            context.fillText(line, canvasWidth / 2, labelOffset + (index + 1) * LABEL_PX_HEIGHT);
+            context.fillText(line, canvasWidth / 2, (index + 1) * LABEL_PX_HEIGHT);
         });
-        texture.repeat.set(canvasWidth / canvasRef.current.width, canvasHeight / canvasRef.current.height);
-        texture.needsUpdate = true;
-        setLabelWidth(canvasWidth);
-        setNumLines(labelLines.length);
-    }, [label, labelSize, maxWidth, paddingBottom, setLabelContext, splitTextIntoLines, texture]);
+
+        const newTexture = new THREE.CanvasTexture(canvasRef.current);
+        newTexture.encoding = THREE.LinearEncoding;
+        newTexture.repeat.set(1, 1);
+        newTexture.needsUpdate = true;
+        setTexture((previous) => {
+            previous?.dispose();
+            return newTexture;
+        });
+    }, [canvasHeight, canvasWidth, getLabelContext, labelLines]);
 
     const scale = useMemo(() => {
-        if (labelWidth) {
+        if (canvasWidth) {
             const pxToWorld = labelSize / LABEL_PX_HEIGHT;
             const scaleX = inverseScale ? inverseScale.x : 1;
             const scaleY = inverseScale ? inverseScale.y : 1;
-            return new THREE.Vector3(labelWidth * pxToWorld / scaleX, (numLines * labelSize + (paddingBottom ?? 0)) / scaleY, 1);
+            return new THREE.Vector3(canvasWidth * pxToWorld / scaleX, canvasHeight * pxToWorld / scaleY, 1);
         } else {
             return undefined;
         }
-    }, [inverseScale, labelSize, labelWidth, numLines, paddingBottom]);
+    }, [canvasHeight, canvasWidth, inverseScale, labelSize]);
+
+    const anchor = useMemo(() => {
+        // Convert world-space padding to canvas pixels
+        const pxPerWorldUnit = LABEL_PX_HEIGHT / labelSize;
+        const paddingPx = (paddingBottom ?? 0) * pxPerWorldUnit;
+        return new THREE.Vector2(0.5, -paddingPx / canvasHeight);
+    }, [canvasHeight, labelSize, paddingBottom]);
 
     return (
-        <sprite position={position} scale={scale} center={ANCHOR} renderOrder={renderOrder} frustumCulled={false}>
+        <sprite position={position} scale={scale} center={anchor} renderOrder={renderOrder} frustumCulled={false}>
             <spriteMaterial attach='material' map={texture} transparent={true} toneMapped={false}
                             depthTest={true} depthWrite={false} alphaTest={0.1}
             />
