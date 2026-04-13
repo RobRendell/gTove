6ce009b94aee38da921880013f295c841d4a4a4b
Rob Rendell <rob.rendell.au@gmail.com>
2026-04-14 07:49:25 +1000

    Fix regression: could no longer save over scenarios.

--- src/presentation/scenarioFileEditor.tsx
+++ src/presentation/scenarioFileEditor.tsx
@@ -1,24 +1,26 @@
 import './scenarioFileEditor.scss';
 
-import {FunctionComponent, useContext, useState} from 'react';
+import {FunctionComponent, useCallback, useContext, useState} from 'react';
 import {useStore} from 'react-redux';
 
 import {FileAPIContextObject} from '../context/fileAPIProvider';
 import {useAsyncSetter} from '../hooks/useAsyncSetter';
-import {FileIndexReducerType} from '../redux/fileIndexReducerTypes';
-import {getAllFilesFromStore} from '../redux/mainReducer';
+import {getAllFilesFromStore, getScenarioFromStore} from '../redux/mainReducer';
 import {jsonToScenarioAndTabletop, scenarioToJson, ScenarioType} from '../util/scenarioUtils';
 import InputButton from './inputButton';
 import {default as RenameFileEditor, RenameFileEditorProps} from './renameFileEditor';
 import TabletopPreviewComponent from './tabletopPreviewComponent';
 
 interface ScenarioFileEditorProps extends RenameFileEditorProps<void, void> {
-    scenario: ScenarioType;
     newFile: boolean;
-    files: FileIndexReducerType;
 }
 
-const ScenarioFileEditor: FunctionComponent<ScenarioFileEditorProps> = (props) => {
+const ScenarioFileEditor: FunctionComponent<ScenarioFileEditorProps> = ({
+                                                                            metadata,
+                                                                            onClose,
+                                                                            getSaveMetadata,
+                                                                            newFile
+                                                                        }) => {
     const fileAPI = useContext(FileAPIContextObject);
     const store = useStore();
 
@@ -26,11 +28,22 @@ const ScenarioFileEditor: FunctionComponent<ScenarioFileEditorProps> = (props) =
     const [fileScenario, setFileScenario] = useState<ScenarioType | undefined>();
 
     useAsyncSetter(setFileScenario, async () => {
-        const json = await fileAPI.getJsonFileContents(props.metadata);
+        const json = await fileAPI.getJsonFileContents(metadata);
         const {fileMetadata} = getAllFilesFromStore(store.getState());
         const [fileScenario] = jsonToScenarioAndTabletop(json as any, fileMetadata);
         return fileScenario;
-    }, [], [fileAPI, props.metadata, store]);
+    }, [], [fileAPI, metadata, store]);
+
+    const onSave = useCallback(() => {
+        const scenario = getScenarioFromStore(store.getState());
+        const [privateScenario] = scenarioToJson(scenario);
+        setSaving(true);
+        return fileAPI.saveJsonToFile(metadata.id, privateScenario)
+            .then(() => {
+                setSaving(false);
+                onClose();
+            });
+    }, [fileAPI, metadata.id, onClose, store]);
 
     return saving ? (
         <div>
@@ -39,24 +52,18 @@ const ScenarioFileEditor: FunctionComponent<ScenarioFileEditorProps> = (props) =
     ) : (
         <RenameFileEditor
             className='scenarioEditor'
-            metadata={props.metadata}
-            onClose={props.onClose}
-            getSaveMetadata={props.getSaveMetadata}
+            metadata={metadata}
+            onClose={onClose}
+            getSaveMetadata={getSaveMetadata}
             controls={[
-                props.newFile ? (
+                newFile ? (
                     <p key='newScenarioInfo'>Your current tabletop layout will be saved in this scenario. To update
                         the scenario later after making further changes to the tabletop, "Edit" the scenario and
                         click the button which will appear on this screen.</p>
                 ) : (
-                    <InputButton type='button' key='saveScenarioOverButton' onChange={() => {
-                        const [privateScenario] = scenarioToJson(props.scenario);
-                        setSaving(true);
-                        return fileAPI.saveJsonToFile(props.metadata.id, privateScenario)
-                            .then(() => {
-                                setSaving(false);
-                                props.onClose();
-                            });
-                    }}>Save current tabletop over this scenario</InputButton>
+                    <InputButton type='button' key='saveScenarioOverButton' onChange={onSave}>
+                        Save current tabletop over this scenario
+                    </InputButton>
                 )
             ]}
         >
